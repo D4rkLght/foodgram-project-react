f40c6bb46a374ff38234ed632c71f2f58637a341
Ярослав Андреев <1frost34eone@gmail.com>
2023-04-02 15:32:15 +0300

    delete useless files and added admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import (Favorite, Subscribe, Ingredient, IngredientAmount,
+                     Recipe, Cart, Tag,)
 
-# Register your models here.
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user')
+    search_fields = ('user',)
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    list_filter = ('author',)
+    search_fields = ('user',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(IngredientAmount)
+class IngredientAmountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ingredient', 'recipe', 'amount',)
+    list_filter = ('recipe', 'ingredient')
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name', 'pub_date')
+    search_fields = ('name',)
+    list_filter = ('pub_date', 'author', 'name', 'tags')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('author', 'recipe')
+    list_filter = ('author',)
+    search_fields = ('author',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
