0a66da5a50ecaedd1d4e46b49fa244192283927a
Ярослав Андреев <1frost34eone@gmail.com>
2023-04-02 15:50:33 +0300

    fixed models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -7,11 +7,20 @@ User = get_user_model()
 
 class Tag(models.Model):
     name = models.CharField(
+        verbose_name='Name',
         max_length=50,
         unique=True
     )
-    color = models.CharField(max_length=7)
-    slug = models.SlugField(unique=True,)
+    color = models.CharField(
+        verbose_name='Color',
+        unique=True,
+        max_length=7
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        max_length=50,
+        unique=True,
+    )
 
     class Meta:
         verbose_name = 'Tag'
@@ -54,11 +63,22 @@ class Recipe(models.Model):
         related_name='recipes',
         verbose_name='Tags'
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=50,)
-    text = models.TextField()
-    cooking_time = models.IntegerField()
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+    )
+    image = models.ImageField(
+        upload_to='images/',
+        verbose_name='Image',
+    )
+    name = models.CharField(
+        max_length=50,
+        db_index=True,
+        verbose_name='Recipe name',
+    )
+    text = models.TextField(verbose_name='Description')
+    cooking_time = models.IntegerField(verbose_name='Cooking time')
     pub_date = models.DateTimeField(
         verbose_name='Publication date',
         auto_now_add=True,
@@ -68,6 +88,11 @@ class Recipe(models.Model):
         verbose_name = 'Recipe'
         verbose_name_plural = 'Recipes'
         ordering = ('-pub_date', )
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'author'],
+                name='unique_recipe')
+        ]
 
     def __str__(self) -> str:
         return self.name
@@ -99,6 +124,9 @@ class IngredientAmount(models.Model):
             models.UniqueConstraint(
                 fields=['recipe', 'ingredient'],
                 name='unique_ingredients')]
+
+    def __str__(self):
+        return f'{self.ingredient} ({self.amount})'
  
 
 class Favorite(models.Model):
@@ -106,11 +134,13 @@ class Favorite(models.Model):
         Recipe,
         related_name='in_favorites',
         on_delete=models.CASCADE,
+        verbose_name='Recipe',
     )
     user = models.ForeignKey(
         User,
         related_name='favorites',
         on_delete=models.CASCADE,
+        verbose_name='Author',
     )
     pub_date = models.DateTimeField(
         verbose_name='Publication date',
@@ -126,11 +156,13 @@ class Cart(models.Model):
         Recipe,
         related_name='in_cart',
         on_delete=models.CASCADE,
+        verbose_name='Recipe',
     )
     author = models.ForeignKey(
         User,
         related_name='cart',
         on_delete=models.CASCADE,
+        verbose_name='User',
     )
     pub_date = models.DateTimeField(
         verbose_name='Publication date',
@@ -139,6 +171,9 @@ class Cart(models.Model):
     class Meta:
         verbose_name = 'Shopping cart'
         verbose_name_plural = 'Shopping carts'
+
+    def __str__(self):
+        return self.recipe
 
 
 class Subscribe(models.Model):
