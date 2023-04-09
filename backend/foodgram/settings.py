0085b110c7517e3bd2cb0ed389dd869cdcbe7153
Ярослав Андреев <1frost34eone@gmail.com>
2023-04-09 15:17:01 +0300

    env

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,9 +1,6 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv(Path(__file__).resolve().parent.parent.parent.joinpath('infra/.env'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,11 +80,11 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='None'),
-        'USER': os.getenv('POSTGRES_USER', default='None'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='None'),
-        'HOST': os.getenv('DB_HOST', default='None'),
-        'PORT': os.getenv('DB_PORT', default='None')
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='postgres')
     }
 }
 
